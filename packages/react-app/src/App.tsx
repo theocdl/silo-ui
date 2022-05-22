d9279d15f9903fb23d40a9c6efa9046696ad6f9d
julienbrg <julien@strat.cc>
2022-05-22 14:18:26 +0200

    fix route name

--- packages/react-app/src/App.tsx
+++ packages/react-app/src/App.tsx
@@ -11,7 +11,7 @@ function App() {
         <Routes>
             <Route path="/" element={<Home/>}/>
             <Route path='/nft/:username' element={<Nft/>}/>
-            <Route path='/issuerDa' element={<IssuerDashboard/>}/>
+            <Route path='/dashboard' element={<IssuerDashboard/>}/>
             <Route path="*" element={<NotFound/>}/>
 
         </Routes>
